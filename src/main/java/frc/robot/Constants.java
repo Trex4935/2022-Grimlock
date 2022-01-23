9dac9f27ed43f11f37d0562f723e96e633dc7e55
Matt B <matbyrd@forthebyrds.com>
2022-01-23 14:51:43 -0500

    added motors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,32 +5,40 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
 
-public static final int leftFrontCanID = 1;
-public static final int leftBackCanID = 2;
-public static final int rightFrontCanID = 3;
-public static final int rightBackCanID = 4;
-public static final int middleLeftCanID = 5;
-public static final int middleRightCanID = 6;
+    public static final int leftFrontCanID = 1;
+    public static final int leftBackCanID = 2;
+    public static final int rightFrontCanID = 3;
+    public static final int rightBackCanID = 4;
+    public static final int middleLeftCanID = 5;
+    public static final int middleRightCanID = 6;
 
-public static final int leftStick = 1;
-public static final int rightStick = 4;
+    public static final int leftStick = 1;
+    public static final int rightStick = 4;
 
     // H drive speed
-public static final double midMotorSpeed = 0.9;
-public static final double motorStop = 0;
+    public static final double midMotorSpeed = 0.9;
+    public static final double motorStop = 0;
 
-public static final int leftHorizontal = 3;
+    public static final int leftHorizontal = 3;
 
-// talon ids
+    // talon ids
     public static final int talon1 = 1;
+
+    // turrent IDs
+    public static final int turretRotationCanID = 98;
+    public static final int turretShooterCanID = 99;
 
 }
---
+++ src/main/java/frc/subsystem/turret.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.subsystem;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class turret extends SubsystemBase {
+  WPI_TalonFX turretShooter;
+  WPI_TalonFX turretRotation;
+
+  /** Creates a new turret. */
+  public turret() {
+
+    turretShooter = new WPI_TalonFX(Constants.turretShooterCanID);
+    turretRotation = new WPI_TalonFX(Constants.turretRotationCanID);
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
