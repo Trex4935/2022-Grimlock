504d9e1dcde618f68fee5043f95f395b283e51db
Matt B <matbyrd@forthebyrds.com>
2022-01-23 16:30:29 -0500

    Minor import fixes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.driveWithController;
+import frc.robot.subsystem.Drivetrain;
 
 public class RobotContainer {
 
@@ -16,33 +17,33 @@ public class RobotContainer {
   public final Drivetrain middle;
   public static XboxController controller;
 
-
   // commands
   private final driveWithController driveWithController;
 
-
   public RobotContainer() {
-  // Drivetrain
-drive = new Drivetrain();
-driveWithController = new driveWithController(drive);
-driveWithController.addRequirements(drive);
-drive.setDefaultCommand(driveWithController);
-middle = new Drivetrain();
+    // Drivetrain
+    drive = new Drivetrain();
+    driveWithController = new driveWithController(drive);
+    driveWithController.addRequirements(drive);
+    drive.setDefaultCommand(driveWithController);
+    middle = new Drivetrain();
 
-controller = new XboxController(0);
+    controller = new XboxController(0);
 
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+  }
 
-
-public Command getAutonomousCommand() {
-  return driveWithController;
+  public Command getAutonomousCommand() {
+    return driveWithController;
   }
 }
--- src/main/java/frc/robot/commands/driveWithController.java
+++ src/main/java/frc/robot/commands/driveWithController.java
@@ -6,20 +6,21 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.Drivetrain;
+import frc.robot.subsystem.Drivetrain;
 import frc.robot.RobotContainer;
 
 public class driveWithController extends CommandBase {
-private final Drivetrain drive;
+  private final Drivetrain drive;
 
   public driveWithController(Drivetrain dt) {
-drive = dt;
-addRequirements(drive);
+    drive = dt;
+    addRequirements(drive);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -30,7 +31,8 @@ addRequirements(drive);
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
