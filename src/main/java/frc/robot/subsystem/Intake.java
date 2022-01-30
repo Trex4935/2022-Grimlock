e6e9f78ad86ea113479d1823c13a348ca1ab4c0d
lowlytides <lmackmayberry@gmail.com>
2022-01-30 13:22:08 -0800

    added intake color sensor

--- src/main/java/frc/robot/subsystem/Intake.java
+++ src/main/java/frc/robot/subsystem/Intake.java
@@ -6,14 +6,19 @@ package frc.robot.subsystem;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Extensions.multiplexedColorSensor;
 
 public class Intake extends SubsystemBase {
 
   WPI_TalonFX intakeMotor;
   WPI_TalonFX magazineMotor1;
   WPI_TalonFX magazineMotor2;
+
+  //intake color sensor
+  private multiplexedColorSensor sensor2;
 
   //// Add 2 smakna
   //// Add color sensor
@@ -28,6 +33,8 @@ public class Intake extends SubsystemBase {
     magazineMotor1.setInverted(false);
     magazineMotor2 = new WPI_TalonFX(Constants.magazineMotor2CanID);
     magazineMotor2.setInverted(false);
+
+    sensor2 = new multiplexedColorSensor(I2C.Port.kOnboard, 2);
 
   }
   // eun intake motor
@@ -56,6 +63,10 @@ public class Intake extends SubsystemBase {
     magazineMotor2.stopMotor();
   }
   
+
+  public void readSensor(){
+    System.out.println(sensor2.getRed() + "," + sensor2.getGreen() + "," + sensor2.getBlue());
+  }
 
   @Override
   public void periodic() {
