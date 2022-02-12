37993ee08e886f91129be0d36843f7fcda901cea
Matt B <matbyrd@forthebyrds.com>
2022-02-12 09:21:28 -0500

    renamed folders to match lower case

--- src/main/java/frc/robot/commands/c_shootWithVision.java
+++ src/main/java/frc/robot/commands/c_shootWithVision.java
@@ -5,7 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Extensions.Limelight;
+import frc.robot.extensions.Limelight;
 import frc.robot.subsystem.Shooter;
 
 public class c_shootWithVision extends CommandBase {
--- src/main/java/frc/robot/subsystem/Drivetrain.java
+++ src/main/java/frc/robot/subsystem/Drivetrain.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Extensions.multiplexedColorSensor;
+import frc.robot.extensions.multiplexedColorSensor;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.SPI;
 
