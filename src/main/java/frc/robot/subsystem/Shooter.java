56a707bb6261ba57f4ce55e5241f1ccf12b4eecd
Robert Smith <darthracer@gmail.com>
2022-03-25 19:33:33 -0400

    Further advancement of Shuffleboard

--- src/main/java/frc/robot/subsystem/Shooter.java
+++ src/main/java/frc/robot/subsystem/Shooter.java
@@ -4,12 +4,19 @@
 
 package frc.robot.subsystem;
 
+import java.util.Map;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -22,6 +29,15 @@ public class Shooter extends SubsystemBase {
 
   // Declare shooter motor
   WPI_TalonFX shooterMotor;
+
+  public ShuffleboardTab matchSettings = Shuffleboard.getTab("Settings");
+  public NetworkTableEntry shooterAdjust = 
+    matchSettings.add("Shooter Speed", 0.0)
+    .withWidget(BuiltInWidgets.kNumberSlider)
+    .withProperties(Map.of("min", -500, "max", 500)) // slider range of -500 to 500
+    .withSize(6, 3) // make the widget 6x3
+    .withPosition(6, 2) // place it in the middle
+    .getEntry();
 
   /** Creates a new Shooter. */
   public Shooter() {
@@ -64,6 +80,9 @@ public class Shooter extends SubsystemBase {
 
     shooterMotor.config_IntegralZone(Constants.kPIDLoopIdx, 0, Constants.kTimeoutMs);
   }
+
+  //Return the Shooter Speed Slider from the Shuffleboard Settings Tab
+  
 
   // converts the ticks to RPM values
   public double tickstoRPM(double ticks) {
@@ -246,7 +265,7 @@ public class Shooter extends SubsystemBase {
 
   public double getSpeedSetPoint(double distance) {
     // RPM = 3.7037 * distance + 2722.2
-    return 3.7037 * distance + 2722.2;
+    return 3.7037 * distance + 2722.2 + shooterAdjust.getDouble(0.0);
   }
 
   public void shooting() {
